c1224397f5177c9d85254004e0ac1811e5f4869e
ilhamhw <80145873+ilhamhw@users.noreply.github.com>
2024-11-30 16:42:47 +0700

    Update login.cy.js

--- cypress/e2e/orangeHRM/Login/login.cy.js
+++ cypress/e2e/orangeHRM/Login/login.cy.js
@@ -12,49 +12,49 @@ describe('Login Feature', () =>{
         loginPage.waitActionsummary();
         loginPage.dashboardPage().should('have.text','Dashboard');
     })
-    it('user Checking Add user', () =>{
-        loginPage.visitPage();
-        loginPage.verifyLoginPage().should('have.text','Login');
-        loginPage.inputUsername().type('Admin');
-        loginPage.inputPassword().type('admin123');
-        loginPage.Actionsummary().as('actionSummary');
-        loginPage.buttonLogin().click();
-        loginPage.waitActionsummary();
-        loginPage.dashboardPage().should('have.text','Dashboard');
-        cy.intercept('GET','https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/admin/users?limit=50&offset=0&sortField=u.userName&sortOrder=ASC').as('userlimit');
-        cy.get('span').contains('Admin').click();
-        cy.wait('@userlimit');
-        cy.get('h6').contains('Admin').should('have.text','Admin');
-        cy.get('[type="button"]').eq(5).click();
-        cy.get('h6').contains('Admin').should('have.text','Admin');
-    })
-    it('user use coloumn search', () =>{
-        loginPage.visitPage();
-        loginPage.verifyLoginPage().should('have.text','Login');
-        loginPage.inputUsername().type('Admin');
-        loginPage.inputPassword().type('admin123');
-        loginPage.Actionsummary().as('actionSummary');
-        loginPage.buttonLogin().click();
-        loginPage.waitActionsummary();
-        loginPage.dashboardPage().should('have.text','Dashboard');
-        cy.get('[placeholder="Search"]').click().type('Admin');
-        cy.get('span').contains('Admin').should('have.text','Admin');
-    })
-    it('user checking about', () =>{
-        loginPage.visitPage();
-        loginPage.verifyLoginPage().should('have.text','Login');
-        loginPage.inputUsername().type('Admin');
-        loginPage.inputPassword().type('admin123');
-        loginPage.Actionsummary().as('actionSummary');
-        loginPage.buttonLogin().click();
-        loginPage.waitActionsummary();
-        loginPage.dashboardPage().should('have.text','Dashboard');
-        cy.get('[alt="profile picture"]').eq(0).click();
-        cy.intercept('GET','https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/core/about').as('about');
-        cy.get('a').contains('About').click();
-        cy.wait('@about');
-        cy.get('h6').contains('About').should('have.text','About');
-    })
+    // it('user Checking Add user', () =>{
+    //     loginPage.visitPage();
+    //     loginPage.verifyLoginPage().should('have.text','Login');
+    //     loginPage.inputUsername().type('Admin');
+    //     loginPage.inputPassword().type('admin123');
+    //     loginPage.Actionsummary().as('actionSummary');
+    //     loginPage.buttonLogin().click();
+    //     loginPage.waitActionsummary();
+    //     loginPage.dashboardPage().should('have.text','Dashboard');
+    //     cy.intercept('GET','https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/admin/users?limit=50&offset=0&sortField=u.userName&sortOrder=ASC').as('userlimit');
+    //     cy.get('span').contains('Admin').click();
+    //     cy.wait('@userlimit');
+    //     cy.get('h6').contains('Admin').should('have.text','Admin');
+    //     cy.get('[type="button"]').eq(5).click();
+    //     cy.get('h6').contains('Admin').should('have.text','Admin');
+    // })
+    // it('user use coloumn search', () =>{
+    //     loginPage.visitPage();
+    //     loginPage.verifyLoginPage().should('have.text','Login');
+    //     loginPage.inputUsername().type('Admin');
+    //     loginPage.inputPassword().type('admin123');
+    //     loginPage.Actionsummary().as('actionSummary');
+    //     loginPage.buttonLogin().click();
+    //     loginPage.waitActionsummary();
+    //     loginPage.dashboardPage().should('have.text','Dashboard');
+    //     cy.get('[placeholder="Search"]').click().type('Admin');
+    //     cy.get('span').contains('Admin').should('have.text','Admin');
+    // })
+    // it('user checking about', () =>{
+    //     loginPage.visitPage();
+    //     loginPage.verifyLoginPage().should('have.text','Login');
+    //     loginPage.inputUsername().type('Admin');
+    //     loginPage.inputPassword().type('admin123');
+    //     loginPage.Actionsummary().as('actionSummary');
+    //     loginPage.buttonLogin().click();
+    //     loginPage.waitActionsummary();
+    //     loginPage.dashboardPage().should('have.text','Dashboard');
+    //     cy.get('[alt="profile picture"]').eq(0).click();
+    //     cy.intercept('GET','https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/core/about').as('about');
+    //     cy.get('a').contains('About').click();
+    //     cy.wait('@about');
+    //     cy.get('h6').contains('About').should('have.text','About');
+    // })
 
 })
 
